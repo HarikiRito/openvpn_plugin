b1bb68e88f7c897f0649a5af46d8ba405a95536b
Hariki <nxh0809@gmail.com>
2023-04-04 00:58:13 +0700

    Init

--- windows/openvpn_plugin.cpp
+++ windows/openvpn_plugin.cpp
@@ -12,48 +12,49 @@
 
 #include <memory>
 #include <sstream>
-
 namespace openvpn_plugin {
 
 // static
-void OpenvpnPlugin::RegisterWithRegistrar(
-    flutter::PluginRegistrarWindows *registrar) {
-  auto channel =
-      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "openvpn_plugin",
-          &flutter::StandardMethodCodec::GetInstance());
+    void OpenvpnPlugin::RegisterWithRegistrar(
+            flutter::PluginRegistrarWindows *registrar) {
+        auto channel =
+                std::make_unique < flutter::MethodChannel < flutter::EncodableValue >> (
+                        registrar->messenger(), "openvpn_plugin",
+                                &flutter::StandardMethodCodec::GetInstance());
 
-  auto plugin = std::make_unique<OpenvpnPlugin>();
+        auto plugin = std::make_unique<OpenvpnPlugin>();
 
-  channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto &call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
+        channel->SetMethodCallHandler(
+                [plugin_pointer = plugin.get()](const auto &call, auto result) {
+                    plugin_pointer->HandleMethodCall(call, std::move(result));
+                });
 
-  registrar->AddPlugin(std::move(plugin));
-}
-
-OpenvpnPlugin::OpenvpnPlugin() {}
-
-OpenvpnPlugin::~OpenvpnPlugin() {}
-
-void OpenvpnPlugin::HandleMethodCall(
-    const flutter::MethodCall<flutter::EncodableValue> &method_call,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
+        registrar->AddPlugin(std::move(plugin));
     }
-    result->Success(flutter::EncodableValue(version_stream.str()));
-  } else {
-    result->NotImplemented();
-  }
-}
+
+    OpenvpnPlugin::OpenvpnPlugin() {}
+
+    OpenvpnPlugin::~OpenvpnPlugin() {}
+
+    void OpenvpnPlugin::HandleMethodCall(
+            const flutter::MethodCall <flutter::EncodableValue> &method_call,
+            std::unique_ptr <flutter::MethodResult<flutter::EncodableValue>> result) {
+        if (method_call.method_name().compare("getPlatformVersion") == 0) {
+            std::string s = "Hello World";
+            s+= " from C++";
+            std::ostringstream version_stream;
+            version_stream << "Windows ";
+            if (IsWindows10OrGreater()) {
+                version_stream << "10+";
+            } else if (IsWindows8OrGreater()) {
+                version_stream << "8";
+            } else if (IsWindows7OrGreater()) {
+                version_stream << "7";
+            }
+            result->Success(flutter::EncodableValue(s));
+        } else {
+            result->NotImplemented();
+        }
+    }
 
 }  // namespace openvpn_plugin
